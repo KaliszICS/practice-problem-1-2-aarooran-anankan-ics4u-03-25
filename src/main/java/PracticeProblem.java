ab35c82b913ac4b1e2d70ee98c510c3dd3d83d0b
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-06 16:30:51 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,53 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
+
+	public static boolean validIndex(int[] intArr, int num) {
+		boolean within = false;
+
+		try {
+			if ((num<intArr.length)) {
+				within = true;
+			}
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+			within = false;
+		}
+
+		return within;
+	}
+
+	public static int divide(int one, int two) {
+
+		int quo = 0;
+
+		try {
+			quo = one/two;	
+		}
+
+		catch (ArithmeticException e) {
+			quo = 0;
+		}
+
+		return quo;
 
 	}
 
-	
+	public static int safeConvertStringtoInt(String word) {
+
+		int con = 0;
+
+		try {
+			con = Integer.valueOf(word);
+		}
+
+		catch (NumberFormatException e) {
+			con = 0;
+		}
+
+		return con;
+
+	}
 
 }
